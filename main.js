b5b20666fe545f57a3c119ae77e24886bfa744cc
marialu270410 <maria.pellozzo.ribeiro@escola.pr.gov.br>
2025-10-29 11:24:22 -0300

    d

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ console.log(avanca)
 avanca.forEach(button => {
     button.addEventListener( 'click',function (){
     const atual = document.querySelector ('.ativo')
-    const proximoPasso = 'passo-'+ this.getAttribute('data-proximo')    
+    const proximoPasso = 'passo-'+ this.getAttribute('data-proximo')  
+    console.log(proximoPasso)  
     atual.classList.remove('ativo')
     document.getElementById(proximoPasso).classList.add('ativo')
 })
